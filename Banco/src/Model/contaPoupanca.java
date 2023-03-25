3b72fa5becd284206208a1867b521b493c67e707
Maria Fernanda Gonçalves <91507393+nandinhaaa@users.noreply.github.com>
2023-03-25 19:59:13 -0300

    Update contaPoupanca.java

--- Banco/src/Model/contaPoupanca.java
+++ Banco/src/Model/contaPoupanca.java
@@ -1,13 +1,17 @@
 package Model;
 
-	public class contaPoupanca extends Conta {
-	    public contaPoupanca(double saldo) {
-	        super(saldo);
-	    }
-	    
-	    @Override
-	    public void atualizar(double taxaPercentual) {
-	        super.atualizar(3 * taxaPercentual);
-	    }
+public class contaPoupanca extends Conta {
+	public contaPoupanca(double saldo, String titular, int numeroConta) {
+		super(saldo, titular, numeroConta);
 	}
 
+	@Override
+	public void atualizar(double taxaPercentual) {
+		super.atualizar(3 * taxaPercentual);
+	}
+
+	@Override
+	public void deposita(double valor) {
+		super.deposita(valor);
+	}
+}
