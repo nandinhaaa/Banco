4e1efa754bc30b9b8e45110107e1b884e15da770
Maria Fernanda Gonçalves <91507393+nandinhaaa@users.noreply.github.com>
2023-03-25 19:59:46 -0300

    Update Main.java

--- Banco/src/View/Main.java
+++ Banco/src/View/Main.java
@@ -7,19 +7,19 @@ import Model.contaPoupanca;
 public class Main {
 
 	public static void main(String[] args) {
-		// criando as contas
-		Conta c = new Conta(1000);
-		contaCorrente cc = new contaCorrente(1000);
-		contaPoupanca cp = new contaPoupanca(1000);
 
-		// atualizando as contas
+		Conta c = new Conta(1000, "Maria", 1);
+		contaCorrente cc = new contaCorrente(1000, "Fernanda", 2);
+		contaPoupanca cp = new contaPoupanca(1000, "Tulio", 3);
+
+		cc.deposita(430.0);
+		cp.deposita(420.0);
+
 		c.atualizar(0.10);
 		cc.atualizar(0.10);
 		cp.atualizar(0.10);
-
-		// imprimindo os saldos atualizados com duas casas decimais
-		System.out.printf("Saldo da conta: %.2f\n", c.getSaldo());
-		System.out.printf("Saldo da conta corrente: %.2f\n", cc.getSaldo());
-		System.out.printf("Saldo da conta poupança: %.2f\n", cp.getSaldo());
+		System.out.printf("Saldo atual da conta de %s: %.2f%n", c.getTitular(), c.getSaldo());
+		System.out.printf("Saldo atual da conta corrente de %s: %.2f%n", cc.getTitular(), cc.getSaldo());
+		System.out.printf("Saldo atual da conta poupanÃ§a de %s: %.2f%n", cp.getTitular(), cp.getSaldo());
 	}
 }
