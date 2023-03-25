7f08c0b3e16a917a11895ea1fd36d0cf26cb27a1
Maria Fernanda Gonçalves <91507393+nandinhaaa@users.noreply.github.com>
2023-03-25 19:58:53 -0300

    Update contaCorrente.java

--- Banco/src/Model/contaCorrente.java
+++ Banco/src/Model/contaCorrente.java
@@ -1,12 +1,17 @@
 package Model;
 
 public class contaCorrente extends Conta {
-    public contaCorrente(double saldo) {
-        super(saldo);
-    }
-    
-    @Override
-    public void atualizar(double taxaPercentual) {
-        super.atualizar(2 * taxaPercentual);
-    }
+	public contaCorrente(double saldo, String titular, int numeroConta) {
+		super(saldo, titular, numeroConta);
+	}
+
+	@Override
+	public void atualizar(double taxaPercentual) {
+		super.atualizar(2 * taxaPercentual);
+	}
+
+	@Override
+	public void deposita(double valor) {
+		super.deposita(valor);
+	}
 }
