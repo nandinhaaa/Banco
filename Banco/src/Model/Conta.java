8b38e2e9ccd6204eeff6f0f585884a391c8ee155
Maria Fernanda Gonçalves <91507393+nandinhaaa@users.noreply.github.com>
2023-03-25 19:58:28 -0300

    Update Conta.java

--- Banco/src/Model/Conta.java
+++ Banco/src/Model/Conta.java
@@ -2,16 +2,31 @@ package Model;
 
 public class Conta {
 	private double saldo;
+	private String titular;
 
-	public Conta(double saldo) {
+	public Conta(double saldo, String titular, int numeroConta) {
 		this.saldo = saldo;
+		this.setTitular(titular);
 	}
 
 	public void atualizar(double taxaPercentual) {
 		this.saldo *= (1 + taxaPercentual / 100);
 	}
 
+	public void deposita(double valor) {
+		saldo += valor;
+	}
+
 	public double getSaldo() {
 		return this.saldo;
 	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
 }
